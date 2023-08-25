d685b0230b61b45bf44e56ee4f326b8dc7cb114f
VladOlevskiy <99341001+VladOlevskiy@users.noreply.github.com>
2023-08-25 22:34:11 +0300

    change routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import { HelmetProvider } from 'react-helmet-async';
-import SharedLayout from './components/SharedLayout';
-import { lazy, useState, useEffect } from 'react';
-import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+import SharedLayout from "./components/SharedLayout";
+import { lazy, useState, useEffect } from "react";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
-const ShoesPage = lazy(() => import('./pages/ShoesPage/ShoesPage'));
-const BasketPage = lazy(() => import('./pages/BasketPage/BasketPage'));
+const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
+const ShoesPage = lazy(() => import("./pages/ShoesPage/ShoesPage"));
+const BasketPage = lazy(() => import("./pages/BasketPage/BasketPage"));
 
 function App() {
   const [shoes, setShoes] = useState([]);
@@ -16,11 +16,11 @@ function App() {
   const [total, setTotal] = useState(0);
   useEffect(() => {
     async function fetchData() {
-      if (localStorage.getItem('my_shoes')) {
-        setAaaa(JSON.parse(localStorage.getItem('my_shoes')));
+      if (localStorage.getItem("my_shoes")) {
+        setAaaa(JSON.parse(localStorage.getItem("my_shoes")));
       }
       try {
-        const response = await axios.get('itemsData.json');
+        const response = await axios.get("itemsData.json");
         const data = response.data;
         setShoes(data);
       } catch (error) {
@@ -37,28 +37,28 @@ function App() {
     setTotal(totalAll);
   }, [aaaa, total]);
 
-  const handleChangeAaaa = async value => {
+  const handleChangeAaaa = async (value) => {
     const newItem = {
       id: value.id + Date.now(),
       name: value.name,
       price: value.price,
       image: value.image,
     };
-    await setAaaa(prev => [...prev, newItem]);
-    localStorage.setItem('my_shoes', JSON.stringify([...aaaa, newItem]));
+    await setAaaa((prev) => [...prev, newItem]);
+    localStorage.setItem("my_shoes", JSON.stringify([...aaaa, newItem]));
   };
 
-  const removeByIdItem = id => {
+  const removeByIdItem = (id) => {
     if (aaaa.length >= 1) {
-      const filteredArray = aaaa.filter(item => item.id !== id);
+      const filteredArray = aaaa.filter((item) => item.id !== id);
       setAaaa(filteredArray);
-      localStorage.setItem('my_shoes', JSON.stringify(filteredArray));
+      localStorage.setItem("my_shoes", JSON.stringify(filteredArray));
     }
   };
   const removeAll = async () => {
     await setTotal[0];
     setAaaa([]);
-    localStorage.setItem('my_shoes', []);
+    localStorage.setItem("my_shoes", []);
   };
 
   return (
